c17821910e6ce88b9c4ed7d84136dd63fd993da0
leovilelati <leovilelati@hotmail.com>
2020-09-03 16:08:12 -0300

    Teste

--- gerador-doc/Controllers/WeatherForecastController.cs
+++ gerador-doc/Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace gerador_doc.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
