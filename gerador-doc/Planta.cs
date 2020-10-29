cd04f5fa17abb983bfa146f812bcf8ae7fe9abde
leovilelati <leovilelati@hotmail.com>
2020-10-29 15:25:45 -0300

    Ajuste

---
+++ gerador-doc/Planta.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gerador_doc
+{
+    public class Planta
+    {
+        public int codigo { get; set; }
+        public string nome { get; set; }
+        public string clima { get; set; }
+    }
+}
